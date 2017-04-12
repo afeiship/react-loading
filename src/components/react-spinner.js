eb6515ca3a0bdf7a166c7536c63e7a32937d0482
afeiship <1290657123@qq.com>
2017-04-12 17:29:38 +0800

    bugfix

--- src/components/react-spinner.js
+++ src/components/react-spinner.js
@@ -1,28 +1,34 @@
 import './style.scss';
+import {PureComponent} from 'react';
 import classNames from 'classnames';
+import PropTypes from 'prop-types';
 
-export default class extends React.Component{
+export default class extends PureComponent{
   static propTypes = {
-    cssClass:React.PropTypes.string,
-    width:React.PropTypes.string,
-    size:React.PropTypes.string,
-    color:React.PropTypes.string
-  }
+    className:PropTypes.string,
+    width:PropTypes.string,
+    size:PropTypes.string,
+    color:PropTypes.string
+  };
 
   static defaultProps={
     size:'28px',
     color:'#26a2ff',
     width:'4px'
-  }
+  };
+
   render(){
+    const {className,size,width,color,...props} = this.props;
     return (
-      <div className={classNames('react-spinner',this.props.cssClass)} style={{
-          width:this.props.size,
-          height:this.props.size,
-          borderWidth:this.props.width,
-          borderTopColor: this.props.color,
-          borderLeftColor: this.props.color,
-          borderBottomColor: this.props.color,
+      <div
+        {...props}
+        className={classNames('react-spinner',className)} style={{
+          width:size,
+          height:size,
+          borderWidth:width,
+          borderTopColor: color,
+          borderLeftColor: color,
+          borderBottomColor: color
         }}></div>
     );
   }
