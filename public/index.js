2010c99eeab500aba6d281810d274b9c1c2798f3
afeiship <1290657123@qq.com>
2021-02-20 16:03:43 +0800

    docs: update demo

--- public/index.js
+++ public/index.js
@@ -34,18 +34,23 @@ class App extends React.Component {
       <ReactDemokit
         className="p-3 app-container"
         url="https://github.com/afeiship/react-loading">
-        <button
-          className="button"
-          onClick={(e) => {
-            ReactLoading.present(
-              () => {
-                console.log('after present');
-              },
-              { children: `[ ${Math.random().toString(36).slice(-6)} ]` }
-            );
-          }}>
-          Show Dynamic Loading.
-        </button>
+        <div className="has-text-centered">
+          <p className="mt-3 mb-3">
+            <img src="https://randomuser.me/api/portraits/lego/7.jpg" alt="" />
+          </p>
+          <button
+            className="button is-primary"
+            onClick={(e) => {
+              ReactLoading.present(
+                () => {
+                  console.log('after present');
+                },
+                { children: `[ ${Math.random().toString(36).slice(-6)} ]` }
+              );
+            }}>
+            <span>Show Dynamic Loading.</span>
+          </button>
+        </div>
       </ReactDemokit>
     );
   }
